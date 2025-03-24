f05e526769cfbac2b39d2fd59277eaa86ee9ce1d
Paraskumar990 <paraskumar270903@gmail.com>
2025-03-24 12:36:31 +0530

    nodejs

--- index.js
+++ index.js
@@ -1,43 +1,14 @@
 const express = require("express");
 const app = express();
-port = 2000
-app.listen(2000,()=>{
-    console.log("server connected",2000);
-})
-app.get('/',(req,res)=>{
-    res.send({
-        status:200,
-        success:true,
-        name:"Paras Kumar"
+// mongodb://localhost:27017/
+port = 2100;
 
-    })
-})
-app.post('/post',(req,res)=>{
-        res.json({
-            status:200,
-            success:true,
-            college:"LKCTC"
-        })
-})
-app.get('/get2',(req,res)=>{
-    res.send({
-        status:200,
-        success:true,
-        Hobby:"FITNESS"
+const db=require ("./server/config/db")
 
-    })
-})
-app.post('/post2',(req,res)=>{
-    res.send({
-        status:200,
-        success:true,
-        Course:"CSE"
-    })
-})
-app.get('/get3',(req,res)=>{
-    res.send({
-        status:200,
-        success:true,
-        Technology:"Mern stack"
-    })
+app.use(express.urlencoded({extended:false}))
+const routes = require("./server/routes/apiRoutes")
+app.use("/apis",routes)
+app.listen(port,()=>{
+    console.log("server connected",port);
+    
 })
